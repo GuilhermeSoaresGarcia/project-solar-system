433993a2d32b3678f62a7511ee7375ac6f4d00f4
Guilherme Soares Garcia <guilherme.garcia@email.com>
2022-04-16 03:52:49 -0300

    Estilização do projeto

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import Title from './Title';
 import missions from '../data/missions';
 import MissionCard from './MissionCard';
+import './Missions.css';
 
 class Missions extends React.Component {
   render() {
     return (
-      <div data-testid="missions">
+      <div className="missions-container" data-testid="missions">
         <Title headline="Missões" />
         <section className="missions">
           {missions.map((item, index) => (
